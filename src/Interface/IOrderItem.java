78e66a3cfc0ea5b4fd48c7a228de9a99fe7a007c
Ducanh-0 <nghiemducanh9494@gmail.com>
2025-12-02 09:21:34 +0700

    Update IOrderItem.java

--- src/Interface/IOrderItem.java
+++ src/Interface/IOrderItem.java
@@ -3,7 +3,7 @@ package Interface;
 import javafx.scene.image.Image;
 
 /* Đại diện cho 1 dòng chi tiết order */
-public interface IOrderItem {    
+public interface IOrderItem extends IMenuItem {    
     String getMenuItemId();     // id món trong menu
     int getQuantity();          // số lượng khách gọi
     double getSubtotal();       // quantity × price
