24cf6953202ab8e332fed7a5c4f3c497b09a342a
chinh1809z <trungchinhht36@gmail.com>
2025-12-02 09:26:40 +0700

    đổi tên một số file

--- src/view/MockTest/MockMenuService.java
+++ src/view/MockTest/MockMenuService.java
@@ -1,36 +1,41 @@
-package view.MockTest;
+package service;
 
-import Interface.IMenuItem;
 import Interface.IMenuService;
-
+import Interface.IMenuItem;
+import model.MenuItem;
+import java.util.ArrayList;
 import java.util.List;
 
-public class MockMenuService implements IMenuService {
+public class MenuService implements IMenuService {
+    private List<IMenuItem> menuList;
+
+    public MenuService() {
+        menuList = new ArrayList<>();
+        menuList.add(new MenuItem("1", "Cà phê đen", 25000, "Drink", "/view/MainScreen/images/ca_phe_den.png"));
+        menuList.add(new MenuItem("2", "Cà phê sữa", 30000, "Drink", "/view/MainScreen/images/ca_phe_sua.png"));
+        menuList.add(new MenuItem("3", "Bạc xỉu", 35000, "Drink", "/view/MainScreen/images/bac_xiu.png"));
+        menuList.add(new MenuItem("4", "Trà đào", 40000, "Drink", "/view/MainScreen/images/tra_dao.png"));
+        menuList.add(new MenuItem("5", "Bánh Croissant", 20000, "Food", "/view/MainScreen/images/banh_croissant.png"));
+        menuList.add(new MenuItem("6", "Bánh Tiramisu", 45000, "Food", "/view/MainScreen/images/banh_tiramisu.png"));
+
+    }
 
     @Override
     public List<IMenuItem> getAllItems() {
-        return List.of(new MockMenuItem("1", "Coffee", 150000.0, "Delicious"),
-                new MockMenuItem("2", "tea", 130000.0, "Delicious")
-                );
+        return menuList; // Trả về danh sách cho Controller
     }
 
     @Override
     public IMenuItem getItemById(String id) {
-        return new MockMenuItem(id, "Coffee", 150000.0, "Delicious");
+        return menuList.stream().filter(i -> i.getId().equals(id)).findFirst().orElse(null);
     }
 
     @Override
     public boolean addMenuItem(IMenuItem item) {
-        return true;
+        return menuList.add(item);
     }
 
-    @Override
-    public boolean updateMenuItem(IMenuItem item) {
-        return true;
-    }
-
-    @Override
-    public boolean deleteMenuItem(String id) {
-        return true;
-    }
+    // Các hàm update/delete bạn có thể để trống (return false) nếu chưa dùng tới
+    @Override public boolean updateMenuItem(IMenuItem item) { return false; }
+    @Override public boolean deleteMenuItem(String id) { return false; }
 }
